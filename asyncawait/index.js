d5f7cbe3e1d3e44994914c742af801e878dc7ecd
Junior Martins <amjr.box@gmail.com>
2021-08-15 22:06:20 -0300

    Update async/ await

--- asyncawait/index.js
+++ asyncawait/index.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8000;
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  //return 'Hello World!';
-});
+const readFile = file =>
+  new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
 
-app.listen(port, () => {
-  console.log('Server listening on port', port);
-});
+const init = async () => {
+  try {
+    const contents1 = await readFile('./files/music1.txt', 'utf8');
+    const contents2 = await readFile('./files/music2.txt', 'utf8');
+
+    console.log(contents1);
+    console.log('');
+    console.log(contents2);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
